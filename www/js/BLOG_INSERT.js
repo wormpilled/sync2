92534b341e41a229c7b1d94c983636241a332ca5
wormpilled <jtchaschowy+wormpill@gmail.com>
2021-11-08 12:58:55 -0800

    a

--- www/js/BLOG_INSERT.js
+++ www/js/BLOG_INSERT.js
@@ -1,3 +1,10 @@
-    $(function(){
-      $(".includedContent").load("b.html"); 
-    });
+document.write('\
+\
+    <h1>Add your HTML code here</h1>\
+\
+     <p>Notice however, that you have to escape LF's with a '\', just like\
+        demonstrated in this code listing.\
+    </p>\
+\
+');
+
